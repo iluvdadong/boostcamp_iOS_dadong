f488a394fa663008194c38ae112d53e59313815f
dadong <iluvdadong@naver.com>
2017-07-09 18:16:55 +0900

    implement a Custom Control

--- week2/FoodTracker/FoodTracker/ViewController.swift
+++ week2/FoodTracker/FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,8 +76,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         present(imagePickerController, animated: true, completion: nil)
         
     }
-    @IBAction func setDefualtLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
+  
 }
 
